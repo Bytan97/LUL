a2a7bcbebf0c1fa6e7cc0b42e136911e27062923
bytan 50541 <enbsfn@gmail.com>
2019-02-03 18:31:20 +0300

    add collision

--- Pong/main.py
+++ Pong/main.py
@@ -2,12 +2,14 @@ import pygame
 import sys
 import os
 from pygame.locals import *
+from random import randint
 
 # init constants
+FLAGS = DOUBLEBUF
 BLACK = 0, 0, 0
 WHITE = 255, 255, 255
 display_size = 800, 600
-FPS = 60
+FPS = 70
 assets = 'assets'
 img = 'img'
 left_text = 'Greeting, use arrows & Q, A to control paddle'
@@ -30,18 +32,54 @@ class Ball(pygame.sprite.Sprite):
     def __init__(self, image, image_rect, x, y):
         super().__init__()
         self.image = pygame.transform.scale(image, (9, 9))
+        self.center_s = (x, y)
         self.size = self.image.get_size()
         self.rect = image_rect
-        self.rect.centerx = x
-        self.rect.centery = y
-        self.velocity = [0, 0]
+        self.rect.size = self.size
+        self.rect.center = self.center_s
+        self.velocity = [5, 5]
+        self.area = pygame.display.get_surface().get_rect()
+        self.hit = 0
 
-    def update(self):
-        self.calc_new_pos()
+    def update(self, DISPLAYSURF, player_l, player_r):
+        new_pos_rect = Rect(self.rect)
+        new_pos_rect.center = (new_pos_rect.centerx + self.velocity[0],
+                               new_pos_rect.centery + self.velocity[1])
+        self.rect = new_pos_rect
 
-    def calc_new_pos(self):
-        self.rect.centerx += self.velocity[0]
-        self.rect.centery += self.velocity[1]
+        if not self.area.contains(new_pos_rect):
+            tl = not self.area.collidepoint(new_pos_rect.topleft)
+            tr = not self.area.collidepoint(new_pos_rect.topright)
+            bl = not self.area.collidepoint(new_pos_rect.bottomleft)
+            br = not self.area.collidepoint(new_pos_rect.bottomright)
+            # handle side collision
+            if (tr and tl) or (br and bl):
+                self.velocity[1] = - self.velocity[1]
+            # handle left player miss
+            if (tl and bl) and not self.hit:
+                self.rect.center = self.center_s
+                self.velocity[0] = -self.velocity[0]
+                flag = randint(0, 1)
+                if flag:
+                    self.velocity[1] = -self.velocity[1]
+            # handle right player miss
+            if (tr and br) and not self.hit:
+                self.rect.center = self.center_s
+                self.velocity[0] = -self.velocity[0]
+                flag = randint(0, 1)
+                if flag:
+                    self.velocity[1] = -self.velocity[1]
+        # handle paddle collision
+        else:
+            player_l.rect.inflate(-3, -3)
+            player_r.rect.inflate(-3, -3)
+
+            if self.rect.colliderect(player_l.rect) == 1 and not self.hit:
+                self.velocity[0] = - self.velocity[0]
+            elif self.rect.colliderect(player_r.rect) == 1 and not self.hit:
+                self.velocity[0] = - self.velocity[0]
+            elif self.hit:
+                self.hit = not self.hit
 
 
 class Paddle(pygame.sprite.Sprite):
@@ -49,14 +87,13 @@ class Paddle(pygame.sprite.Sprite):
         super().__init__()
         self.area = pygame.display.get_surface().get_rect()
         self.image = image
-        self.size = self.image.get_size()
         self.rect = image_rect
-        self.rect.centerx = x
-        self.rect.centery = y
+        self.rect.center = x, y
         self.velocity = 12
         self.state = ''
 
-    def update(self):
+    def update(self, DISPLAYSURF):
+        pygame.draw.rect(DISPLAYSURF, (255, 0, 0), self.rect, 1)
         pass
 
     def move_up(self):
@@ -77,7 +114,9 @@ class Paddle(pygame.sprite.Sprite):
 def main():
     # main initialize
     pygame.init()
-    DISPLAYSURF = pygame.display.set_mode(display_size)
+    DISPLAYSURF = pygame.display.set_mode(display_size, FLAGS)
+    pygame.event.set_allowed([QUIT, KEYDOWN])
+    DISPLAYSURF.set_alpha(None)
     load = 1
     # waiting until load assets
     while load:
@@ -87,7 +126,7 @@ def main():
         load = 0
     # basic init
     pygame.display.set_caption('Pong')
-    pygame.mouse.set_visible(0)
+    # pygame.mouse.set_visible(0)
 
     fps_clock = pygame.time.Clock()
 
@@ -124,14 +163,20 @@ def main():
     pygame.display.flip()
     getTicksLastFrame = 0
     sums = 0
+    #########################
+    ########################
     # main loop
     while True:
-        DISPLAYSURF.fill(BLACK)
+        # DISPLAYSURF.fill(BLACK)
 
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
+
+        DISPLAYSURF.blit(background, ball.rect, ball.rect)
+        DISPLAYSURF.blit(background, player_l.rect, player_l.rect)
+        DISPLAYSURF.blit(background, player_r.rect, player_r.rect)
 
         # handle users control
         keys = pygame.key.get_pressed()
@@ -149,15 +194,15 @@ def main():
             player_r.move_down()
 
         # draw
-        ball.update()
-        player_l.update()
-        player_r.update()
+        ball.update(DISPLAYSURF, player_l, player_r)
+        player_l.update(DISPLAYSURF)
+        player_r.update(DISPLAYSURF)
         ballspite.draw(DISPLAYSURF)
         player_l_sprite.draw(DISPLAYSURF)
         player_r_sprite.draw(DISPLAYSURF)
         pygame.display.flip()
+        print(fps_clock)
         fps_clock.tick(FPS)
-
 
 if __name__ == '__main__':
     main()
